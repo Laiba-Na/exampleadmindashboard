52d9bd6bf147ba4f438cc1da43e648255ff59c46
Laiba Nazir <162683597+Laiba-Na@users.noreply.github.com>
2025-08-12 00:02:38 +0500

    calendar initiated

---
+++ src/scenes/calendar/index.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { formatDate } from '@fullcalendar/core';
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
+import {Box , List , ListItem, ListItemText, Typography , useTheme} from "@mui/material";
+import { tokens } from "../../theme";
+import Header from "../../Components/Header"
+import FullCalendar from '@fullcalendar/react';
+import interactionPlugin from '@fullcalendar/interaction';
+
+
+
+const Calendar = () => {
+
+}
+
+export default Calendar;
+
+
