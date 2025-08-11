fec6cb4650a3597e343c1b53fd72b7fa84d6ff2d
Laiba Nazir <162683597+Laiba-Na@users.noreply.github.com>
2025-08-11 23:14:03 +0500

    Invoices and team page setup

--- src/scenes/invoices/index.jsx
+++ src/scenes/invoices/index.jsx
@@ -1,33 +1,38 @@
 
 import { DataGrid  } from '@mui/x-data-grid';
-import {mockDataContacts} from '../../data/mock';
+import {mockDataInvoices} from '../../data/mock';
 import {tokens} from '../../theme';
 import Header from '../../Components/Header'
-import { useTheme } from '@mui/material/styles';
-import { Box } from '@mui/material';
+import { Box , Typography , useTheme} from '@mui/material';
+import { render } from '@testing-library/react';
 
 
-const Contacts = () => {
+const Invoices = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
     const columns = [
-        {field: "id", headerName: "ID" , flex :0.5},
-        {field:"registrarId", headerName: "Registrar ID" },
+        {field: "id", headerName: "ID" },
         {field: "name", headerName: "Name" , flex: 1, cellClassName: "name-column--cell"},
-        {field:'age' , headerName: "Age" , type: 'number', headerAlign: 'left', align: 'left'},
         {field: "phone", headerName: "Phone Number", flex: 1},
         {field: "email", headerName: "Email", flex: 1},
-        {field: "address", headerName: "Address", flex: 1},
-        {field: "city", headerName: "City", flex: 1},
-        {field: "zipCode", headerName: "Zip Code", flex: 1},
-        
+        {field: "date", headerName: "Date", flex: 1},
+        {field: "cost", headerName: "Cost", flex: 1 ,
+            renderCell: (params) => {
+                return (
+                    <Typography color={colors.Third[500]}>
+                        ${params.row.cost}
+                    </Typography>
+                )
+            }
+        },
+
         
     ]
     
     return(
         <Box m={'10px 20px'}>
-            <Header title="CONTACTS" subtitle = 'List of Contacts for Future Reference' />
+            <Header title="INVOICES" subtitle = 'List of Invoice Balances' />
             <Box
                 m='10px 0 0 0'
                 height={ '75vh'}
@@ -50,16 +55,16 @@ const Contacts = () => {
                         borderTop: 'none',
                         backgroundColor: colors.Secondary[600],
                     },
-                    "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
-                        color: `${colors.Secondary[100]} !important`,
+                    "& .MuiCheckbox-root": {
+                        color: `${colors.Secondary[500]} !important`,
                     },
                 }}
                
             >
                 <DataGrid
-                    rows={mockDataContacts}
+                checkboxSelection
+                    rows={mockDataInvoices}
                     columns={columns}
-                   showToolbar
                 >
 
                 </DataGrid>
@@ -70,4 +75,4 @@ const Contacts = () => {
 
 }
 
-export default Contacts;
+export default Invoices;
